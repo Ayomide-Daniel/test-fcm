afe4b116750a94f8a3742cdf0a7804f2d825a3cb
Brown <ayomidedaniel00@gmail.com>
2023-02-25 00:14:05 +0100

    feat: sw

---
+++ firebase-messaging-sw.js
@@ -0,0 +1,44 @@
+// push notifications using firebase cloud messaging
+import { initializeApp } from "firebase/app";
+import { getMessaging, getToken } from "firebase/messaging";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDac77e7RyXxZ4hFYqsoH9N41krF1U7fTE",
+  authDomain: "obtainly-local.firebaseapp.com",
+  projectId: "obtainly-local",
+  storageBucket: "obtainly-local.appspot.com",
+  messagingSenderId: "385414976213",
+  appId: "1:385414976213:web:909bdbe899c84cbc55885c",
+};
+
+const app = initializeApp(firebaseConfig);
+
+const messaging = getMessaging(app);
+
+// request permission to send notifications
+
+// get token in the form of promise
+getToken(messaging, {
+  vapidKey:
+    "BMHgsxsPzk0IZNS53nnzG9TlhrZ5aH7x63aW-eNom31FPTNmfAeMGIYUAXDnoWWqK5AXwk9ZEMNnkF5iZU0aPf4",
+})
+  .then((currentToken) => {
+    console.log(currentToken);
+    if (currentToken) {
+      // send token to server
+    }
+  })
+  .catch((err) => {
+    console.log("An error occurred while retrieving token. ", err);
+  });
+
+requestPermission();
+
+function requestPermission() {
+  console.log("Requesting permission...");
+  Notification.requestPermission().then((permission) => {
+    if (permission === "granted") {
+      console.log("Notification permission granted.");
+    }
+  });
+}
