c6f16e47b39ac3b4c2c7ad98bae9ae50b9bc22d3
Brown <ayomidedaniel00@gmail.com>
2023-02-25 12:37:32 +0100

    fix 5

--- src/js/app.js
+++ src/js/app.js
@@ -53,13 +53,13 @@ onMessage(messaging, (payload) => {
 
 requestPermission();
 
-// if ("serviceWorker" in navigator) {
-//   navigator.serviceWorker
-//     .register("./firebase-messaging-sw.js")
-//     .then(function (registration) {
-//       console.log("Registration successful, scope is:", registration.scope);
-//     })
-//     .catch(function (err) {
-//       console.log("Service worker registration failed, error:", err);
-//     });
-// }
+if ("serviceWorker" in navigator) {
+  navigator.serviceWorker
+    .register("../../firebase-messaging-sw.js")
+    .then(function (registration) {
+      console.log("Registration successful, scope is:", registration.scope);
+    })
+    .catch(function (err) {
+      console.log("Service worker registration failed, error:", err);
+    });
+}
