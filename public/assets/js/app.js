feaff56e9cf4c2f3c786feb870857b17a4724a9b
Brown <ayomidedaniel00@gmail.com>
2023-02-25 12:11:38 +0100

    fix

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -1,5 +1,17 @@
-import { app } from "../../firebase-messaging-sw.js";
+// push notifications using firebase cloud messaging
+import { initializeApp } from "firebase/app";
 import { getMessaging, getToken, onMessage } from "firebase/messaging";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDac77e7RyXxZ4hFYqsoH9N41krF1U7fTE",
+  authDomain: "obtainly-local.firebaseapp.com",
+  projectId: "obtainly-local",
+  storageBucket: "obtainly-local.appspot.com",
+  messagingSenderId: "385414976213",
+  appId: "1:385414976213:web:909bdbe899c84cbc55885c",
+};
+
+const app = initializeApp(firebaseConfig);
 
 // request permission to send notifications
 const messaging = getMessaging(app);
@@ -23,8 +35,7 @@ function requestPermission() {
         .catch((err) => {
           console.log("An error occurred while retrieving token. ", err);
         });
-    }
-    else {
+    } else {
       console.log("Unable to get permission to notify.");
     }
   });
