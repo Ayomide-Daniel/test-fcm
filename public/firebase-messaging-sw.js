7b1f14e9906d56f8065cdd64108548aace7299b9
Brown <ayomidedaniel00@gmail.com>
2023-02-25 12:28:00 +0100

    fix 3

---
+++ public/firebase-messaging-sw.js
@@ -0,0 +1,31 @@
+// Scripts for firebase and firebase messaging
+importScripts(
+  "https://www.gstatic.com/firebasejs/<v9+>/firebase-app-compat.js"
+);
+importScripts(
+  "https://www.gstatic.com/firebasejs/<v9+>/firebase-messaging-compat.js"
+);
+const firebaseConfig = {
+  apiKey: "AIzaSyDac77e7RyXxZ4hFYqsoH9N41krF1U7fTE",
+  authDomain: "obtainly-local.firebaseapp.com",
+  projectId: "obtainly-local",
+  storageBucket: "obtainly-local.appspot.com",
+  messagingSenderId: "385414976213",
+  appId: "1:385414976213:web:909bdbe899c84cbc55885c",
+};
+
+const app = initializeApp(firebaseConfig);
+
+// request permission to send notifications
+const messaging = getMessaging(app);
+
+messaging.onBackgroundMessage(function (payload) {
+  console.log("Received background message ", payload);
+
+  const notificationTitle = payload.notification.title;
+  const notificationOptions = {
+    body: payload.notification.body,
+  };
+
+  self.registration.showNotification(notificationTitle, notificationOptions);
+});
