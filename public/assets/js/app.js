c94c19cd32e6b01d54ab60c46d4320278b237156
Brown <ayomidedaniel00@gmail.com>
2023-02-25 11:48:32 +0100

    feat: move sw

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -1,12 +1,41 @@
-import "./firebase-messaging-sw.js";
+import { app } from "../../firebase-messaging-sw.js";
+// request permission to send notifications
+const messaging = getMessaging(app);
+
+function requestPermission() {
+  console.log("Requesting permission...");
+  Notification.requestPermission().then((permission) => {
+    if (permission === "granted") {
+      console.log("Notification permission granted.");
+      // get token in the form of promise
+      getToken(messaging, {
+        vapidKey:
+          "BMHgsxsPzk0IZNS53nnzG9TlhrZ5aH7x63aW-eNom31FPTNmfAeMGIYUAXDnoWWqK5AXwk9ZEMNnkF5iZU0aPf4",
+      })
+        .then((currentToken) => {
+          console.log("currentToken", currentToken);
+          if (currentToken) {
+            // send token to server
+          }
+        })
+        .catch((err) => {
+          console.log("An error occurred while retrieving token. ", err);
+        });
+    }
+    else {
+      console.log("Unable to get permission to notify.");
+    }
+  });
+}
+
 // Handle incoming messages. Called when:
 // - a message is received while the app has focus
 // - the user clicks on an app notification created by a service worker
 //   `messaging.onBackgroundMessage` handler.
-import { getMessaging, onMessage } from "firebase/messaging";
 
-const messaging = getMessaging();
 onMessage(messaging, (payload) => {
   console.log("Message received. ", payload);
   // ...
 });
+
+requestPermission();
---
+++ public/firebase-messaging-sw.js
@@ -0,0 +1,14 @@
+// push notifications using firebase cloud messaging
+import { initializeApp } from "firebase/app";
+import { getMessaging, getToken } from "firebase/messaging";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDac77e7RyXxZ4hFYqsoH9N41krF1U7fTE",
+  authDomain: "obtainly-local.firebaseapp.com",
+  projectId: "obtainly-local",
+  storageBucket: "obtainly-local.appspot.com",
+  messagingSenderId: "385414976213",
+  appId: "1:385414976213:web:909bdbe899c84cbc55885c",
+};
+
+export const app = initializeApp(firebaseConfig);
