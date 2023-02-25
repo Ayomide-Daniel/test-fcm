edfe7642fb94d63a0718cc444c452de01b7fdfbb
Brown <ayomidedaniel00@gmail.com>
2023-02-25 11:09:53 +0100

    feat: add missing app js

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require("path");
 
 module.exports = {
   // The entry point file described above
-  entry: "./src/app.js",
+  entry: "./public/assets/js/app.js",
   // The location of the build folder described above
   output: {
     path: path.resolve(__dirname, "dist"),
